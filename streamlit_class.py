648123db85da6844e54c10b0b1ae51be087670d2
harishmuh <149560161+harishmuh@users.noreply.github.com>
2024-12-01 17:59:30 +0700

    Add files via upload

---
+++ streamlit_class.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("Hello world")
+
+#st.set_page_config(layout='wide', initial_sidebar_state = 'expanded', page_title='Belajar streamlit')
+
+
+#streamlit run <nama app.py>
+
