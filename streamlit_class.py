55c31431ebce8c92eaaec5618838e11af9cb5555
harishmuh <149560161+harishmuh@users.noreply.github.com>
2024-12-01 22:12:22 +0700

    Delete streamlit_class.py

--- streamlit_class.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.write("Hello world")
-
-#st.set_page_config(layout='wide', initial_sidebar_state = 'expanded', page_title='Belajar streamlit')
-
-
-#streamlit run <nama app.py>
-
